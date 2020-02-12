52f1352f76286d50a79da1101a5b7765d6cd5ccc
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-12 16:30:40 +0530

    Refactors for loop

--- arrayslice/sum.go
+++ arrayslice/sum.go
@@ -2,8 +2,8 @@ package arrayslice
 
 // Sum takes an array of integers and returns its sum
 func Sum(arr [5]int) (result int) {
-	for i := 0; i < len(arr); i++ {
-		result += arr[i]
+	for _, num := range arr {
+		result += num
 	}
 	return
 }
