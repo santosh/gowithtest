8608dbfb25f34b2a41c5ae08895f168bbbc7c683
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-13 15:01:23 +0530

    Alters Perimeter & Area to use Rectangle..
    ..modifies respective Tests as well.

--- struct-methods-interfaces/shapes.go
+++ struct-methods-interfaces/shapes.go
@@ -2,11 +2,18 @@
 package shapes
 
 // Perimeter takes float64 width and heigth and returns perimeter of a rectangle.
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area takes float64 width and heigth and returns calculated
-func Area(width, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+// Rectangle is a quadrilateral with same length of opposite sides.
+// By this defition a square is also a rectangle.
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
--- struct-methods-interfaces/shapes_test.go
+++ struct-methods-interfaces/shapes_test.go
@@ -3,7 +3,8 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,7 +13,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(5.0, 5.0)
+	rectangle := Rectangle{5.0, 5.0}
+	got := Area(rectangle)
 	want := 25.0
 
 	if got != want {
