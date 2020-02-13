48ca42d42baec7d10be796d5c7424917547c4f17
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-13 13:21:42 +0530

    Adds SumAll and TestSumAll

--- arrayslice/sum.go
+++ arrayslice/sum.go
@@ -7,3 +7,12 @@ func Sum(arr []int) (result int) {
 	}
 	return
 }
+
+// SumAll takes multiple slice and returns a single slice.
+// Each index representing sum of each slice passed.
+func SumAll(numToSum ...[]int) (sums []int) {
+	for _, numbers := range numToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
--- arrayslice/sum_test.go
+++ arrayslice/sum_test.go
@@ -1,6 +1,10 @@
 package arrayslice
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -13,4 +17,30 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func ExampleSum() {
+	foo := []int{1, 2}
+	fmt.Println(Sum(foo))
+	// Output: 3
+}
+
+func TestSumAll(t *testing.T) {
+	foo := []int{1, 2, 3}
+	bar := []int{5, 6}
+
+	got := SumAll(foo, bar)
+	want := []int{6, 11}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func ExampleSumAll() {
+	foo := []int{1, 2}
+	bar := []int{5, 6}
+	fmt.Println(SumAll(foo, bar))
+
+	// Output: [3 11]
 }
