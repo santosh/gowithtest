d2238134b962554293930c4357d8668316f8434d
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-17 17:47:52 +0530

    Refactor Greet..
    ..Greet can now write to http.ResponseWriter

--- injection/di.go
+++ injection/di.go
@@ -2,11 +2,20 @@
 package injection
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 // Greet ("santosh") -> "Hello, santosh"
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func myGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	http.ListenAndServe(":8500", http.HandlerFunc(myGreeterHandler))
 }
