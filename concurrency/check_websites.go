d3a01d665763328f6c0f234196b6e940f67b0eea
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-19 18:04:58 +0530

    Uses channels to succintly pass data..
    ..from the parts which can run concurrently and those which can't.

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -3,22 +3,30 @@
 // go routines are thread managed by go virtual machine (and are not system threads).
 package concurrency
 
-import "time"
-
+// WebsiteChecker is used in CheckWebsites.
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 // CheckWebsites returns a map of each URL checked to a boolean value
 // true for a good response, false for a bad response.
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
-	time.Sleep(1 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 
 	return results
 }
