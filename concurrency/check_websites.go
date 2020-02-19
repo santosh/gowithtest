742ff389ffd67d1a0d7106e607677eb8edb3bebd
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-19 17:50:25 +0530

    Uses go routines for each url; go managed threads
    We still have data race.

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -1,6 +1,9 @@
 // Package concurrency demonstrate optimization with concurrency using go routines.
+//
 // go routines are thread managed by go virtual machine (and are not system threads).
 package concurrency
+
+import "time"
 
 type WebsiteChecker func(string) bool
 
@@ -10,8 +13,12 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			results[u] = wc(u)
+		}(url)
 	}
+
+	time.Sleep(1 * time.Second)
 
 	return results
 }
