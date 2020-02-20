26632e65fdd9e5c36023c28008b66c3edf91ca0a
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-20 12:24:58 +0530

    Facilitate Racer to timeout after 10s

--- select/racer.go
+++ select/racer.go
@@ -1,17 +1,20 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
 // Racer runs http.Get() on both passed URLs and returns the one responded first.
-func Racer(url1, url2 string) (winner string) {
+func Racer(url1, url2 string) (winner string, err error) {
 	select {
 	case <-ping(url1):
-		return url1
+		return url1, nil
 	case <-ping(url2):
-		return url2
+		return url2, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", url1, url2)
 	}
 }
 
