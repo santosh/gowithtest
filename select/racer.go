71401de2a7e482ea992165db99f8d6963ae91dc7
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-20 12:02:17 +0530

    Use go routines & listen to both chans w/ select

--- select/racer.go
+++ select/racer.go
@@ -7,13 +7,21 @@ import (
 
 // Racer runs http.Get() on both passed URLs and returns the one responded first.
 func Racer(url1, url2 string) (winner string) {
-	aDuration := measureResponseTime(url1)
-	bDuration := measureResponseTime(url2)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(url1):
 		return url1
+	case <-ping(url2):
+		return url2
 	}
-	return url2
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
 
 func measureResponseTime(url string) time.Duration {
