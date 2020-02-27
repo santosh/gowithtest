8449263d0c2dd0d554248b1b3a37fffeb6bc56ae
Santosh Kumar <sntshkmr60@gmail.com>
2020-02-27 13:41:39 +0530

    Refactor TestRomanNumerals to Table based test

--- roman/numeral.go
+++ roman/numeral.go
@@ -2,5 +2,8 @@ package roman
 
 // ConvertToRoman converts Arabic numerals to Roman.
 func ConvertToRoman(arabic int) string {
+	if arabic == 2 {
+		return "II"
+	}
 	return "I"
 }
--- roman/numeral_test.go
+++ roman/numeral_test.go
@@ -3,19 +3,21 @@ package roman
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-		assertEqual(t, got, want)
-	})
-
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		assertEqual(t, got, want)
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			assertEqual(t, got, test.Want)
+		})
+	}
 }
 
 func assertEqual(t *testing.T, got, want string) {
